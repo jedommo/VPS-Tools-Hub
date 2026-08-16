3c955462eb7c42e83230a3237ecd094406bb09ec
MaMDoUH <43824473+jedommo@users.noreply.github.com>
2026-08-16 22:36:48 +0300

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "================================="
+echo "Hello from MAMDOUH OS 🚀"
+echo "The script is working successfully!"
+echo "Server: $(hostname)"
+echo "User: $(whoami)"
+echo "Date: $(date)"
+echo "================================="
